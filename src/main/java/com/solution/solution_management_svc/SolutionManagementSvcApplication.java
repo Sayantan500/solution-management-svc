ea3194e1cf8bc8e00303892544f67846344efd6d
USER <1905707@kiit.ac.in>
2022-10-27 12:01:55 +0530

    Initial Commit...

---
+++ src/main/java/com/solution/solution_management_svc/SolutionManagementSvcApplication.java
@@ -0,0 +1,13 @@
+package com.solution.solution_management_svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolutionManagementSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolutionManagementSvcApplication.class, args);
+	}
+
+}
