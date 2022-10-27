056a8b574c1cb2d0ce510499b0b692d73b6fdfe3
USER <1905707@kiit.ac.in>
2022-10-27 20:57:55 +0530

    Connected to database...

--- src/main/java/com/solution/solution_management_svc/controller/Controller.java
+++ src/main/java/com/solution/solution_management_svc/controller/Controller.java
@@ -1,6 +1,8 @@
 package com.solution.solution_management_svc.controller;
 
 import com.solution.solution_management_svc.models.Solution;
+import com.solution.solution_management_svc.persistence.SolutionsDAO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,23 +12,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/solutions")
 public class Controller
 {
+    @Autowired
+    private SolutionsDAO solutionsDAO;
 
     @GetMapping
-    public ResponseEntity<Object> getSolutionBasedOnQueryParam(
+    public ResponseEntity<Solution> getSolutionBasedOnQueryParam(
             @RequestParam(name = "issue-id") String issueID
     )
     {
-        //Todo : get all the solutions for the specified issue id and return as list.
-        return null;
+        Solution solution = solutionsDAO.getSolutionByIssueId(issueID);
+        HttpStatus httpStatus = solution!=null? HttpStatus.OK:HttpStatus.NOT_FOUND;
+        return new ResponseEntity<>(solution,httpStatus);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Object> getSolutionBasedOnSolutionId(
+    public ResponseEntity<Solution> getSolutionBasedOnSolutionId(
             @PathVariable(name = "id") String solutionId
     )
     {
-        //Todo : get the solution with the specified id and return the solution object.
-        return null;
+        Solution solution = solutionsDAO.getSolutionById(solutionId);
+        HttpStatus httpStatus = solution == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<>(solution,httpStatus);
     }
 
     @PostMapping("/new")
@@ -34,8 +40,9 @@ public class Controller
             @RequestBody Solution newSolution
     )
     {
-        //Todo : get the solution object and save in the database.
-        return null;
+        newSolution = solutionsDAO.createNewSolution(newSolution);
+        HttpStatus httpStatus = newSolution != null ? HttpStatus.CREATED : HttpStatus.NO_CONTENT;
+        return new ResponseEntity<>(newSolution, httpStatus);
     }
 
     @DeleteMapping("/{id}")
@@ -43,7 +50,10 @@ public class Controller
             @PathVariable(name = "id") String solutionId
     )
     {
-        //Todo : delete the solution having id as solutionId and return the appropriate response.
-        return null;
+        HttpStatus httpStatus =
+                solutionsDAO.deleteSolution(solutionId) ?
+                        HttpStatus.NO_CONTENT :
+                        HttpStatus.NOT_FOUND;
+        return new ResponseEntity<>(null,httpStatus);
     }
 }
